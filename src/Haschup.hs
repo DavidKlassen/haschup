40ec5a8efde8efaeddcd8ccc56ed6f5e0fff3b0d
David Klassen <f0rk.tt@gmail.com>
2014-06-01 01:35:49 +0700

    Moved sources to src dir

--- src/Haschup.hs
+++ src/Haschup.hs
@@ -1,14 +1,11 @@
 --
 -- Copyright (c) 2014 David Klassen
 --
-module Haschup(main) where
+module Main(main) where
 
 import System.Environment
  
 -- | 'main' runs the main program
 main :: IO ()
 main = getArgs >>= print . hello . head
- 
--- | 'hello' adds "Hello! " prefix to the passed string
-hello :: String -> String
-hello s = "Hello! " ++ s
+    where hello s = "Hello! " ++ s
