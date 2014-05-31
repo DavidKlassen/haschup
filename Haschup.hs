5ae98f19829ebd6b33a1da7c231bd5914fdaab36
David Klassen <f0rk.tt@gmail.com>
2014-06-01 01:11:28 +0700

    Added module declaration and haddock comment

--- Haschup.hs
+++ Haschup.hs
@@ -1,10 +1,14 @@
 --
 -- Copyright (c) 2014 David Klassen
 --
+module Haschup(main) where
+
 import System.Environment
  
 -- | 'main' runs the main program
 main :: IO ()
 main = getArgs >>= print . hello . head
  
+-- | 'hello' adds "Hello! " prefix to the passed string
+hello :: String -> String
 hello s = "Hello! " ++ s
