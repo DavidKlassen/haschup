06a7ad8ba072aedeb5722068f867120e153702cc
David Klassen <f0rk.tt@gmail.com>
2014-06-03 19:59:25 +0700

    Basic playlist parser

--- src/Haschup.hs
+++ src/Haschup.hs
@@ -1,11 +1,20 @@
---
--- Copyright (c) 2014 David Klassen
---
-module Main(main) where
+-- | Main module
+module Main where
 
-import System.Environment
- 
--- | 'main' runs the main program
+import qualified Data.ByteString as BS
+import qualified Data.Text as T
+import Text.Playlist
+
+-- | Reads playlist from stdin
+readPlaylist :: Format -> IO Playlist
+readPlaylist fmt = do
+  content <- BS.getContents
+  case parsePlaylist fmt content of
+    Left err -> fail $ "failed to parse playlist on stdin: " ++ err
+    Right x  -> return x
+
+-- | Application entry point
 main :: IO ()
-main = getArgs >>= print . hello . head
-    where hello s = "Hello! " ++ s
+main = do
+    playlist <- readPlaylist M3U
+    mapM_ (putStrLn . T.unpack . trackURL) playlist
