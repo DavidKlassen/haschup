b0829c659dff1c7771970234185562d19a2f285d
David Klassen <f0rk.tt@gmail.com>
2014-06-09 19:09:03 +0600

    Refactoring

--- src/Haschup.hs
+++ src/Haschup.hs
@@ -9,19 +9,23 @@ import qualified Data.ByteString.Lazy as L
 import Control.Monad.IO.Class (liftIO)
 import Text.Playlist
 
+-- | Fetches the playlist from the given url
+fetchPlaylist :: String -> IO BS.ByteString
+fetchPlaylist urlString = do
+    case parseUrl urlString of
+        Nothing -> fail "Sorry, invalid URL"
+        Just req -> withManager $ \manager -> do
+            res <- httpLbs req manager
+            return $ L.toStrict $ responseBody res
+
 -- | Application entry point
 main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [urlString] ->
-            case parseUrl urlString of
-                Nothing -> putStrLn "Sorry, invalid URL"
-                Just req -> withManager $ \manager -> do
-                    res <- httpLbs req manager
-                    let body = responseBody res
-                    let playlist = parsePlaylist M3U $ L.toStrict body
-                    case playlist of
-                        Left err -> fail $ "failed to parse playlist on stdin: " ++ err
-                        Right x  -> liftIO $ putStr $ unlines $ map (T.unpack . trackURL) x
+        [urlString] -> do
+            content <- fetchPlaylist urlString
+            case parsePlaylist M3U content of
+                Left err -> fail $ "failed to parse playlist on stdin: " ++ err
+                Right x  -> liftIO $ putStr $ unlines $ map (T.unpack . trackURL) x
         _ -> putStrLn "Sorry, please provide exactly one URL"
